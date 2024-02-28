c02f92b43cd933917a11e79110ca37e4fadb7d9d
matrasulov77 <matrasulovmatrasul752@gmail.com>
2024-02-28 12:22:31 +0500

    Header end

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import F_Header from '../Header/f_Header'
 import S_Header from '../Header/s_Header'
+import T_Header from '../Header/t_Header'
 
 const Header = () => {
   return (
     <div>
       <F_Header/>
       <S_Header/>
+      <T_Header/>
     </div>
   )
 }
